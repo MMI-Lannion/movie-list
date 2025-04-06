6fa3d63c3a5658e0712ee1b80177a7049b2dce30
pierrebueno <80826481+pierrebueno@users.noreply.github.com>
2025-04-06 08:33:06 +0200

    fixed a bad movie ref

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,30 @@ import './App.css'
 
 function App() {
 
-  let movies = moviesList.map(
-    m =>
-      <Movie
-        warning = {m.warning}
-        bu = {m.bu}
-        title = {m.title}
-        plot = {m.plot}
-        orig_title = {m.orig_title}
-        poster = {m.poster}
-        director = {m.director}
-        oscar = {awards.oscar.map(s => s.toLocaleLowerCase()).includes(m.title.toLocaleLowerCase())}
-        cannes = {awards.cannes.map(s => s.toLocaleLowerCase()).includes(m.title.toLocaleLowerCase())}
-        berlin = {awards.berlin.map(s => s.toLocaleLowerCase()).includes(m.title.toLocaleLowerCase())}
-        mostra = {awards.mostra.map(s => s.toLocaleLowerCase()).includes(m.title.toLocaleLowerCase())}
-        cesar = {awards.cesar.map(s => s.toLocaleLowerCase()).includes(m.title.toLocaleLowerCase())}
-        year = {m.year}
-        runtime = {m.runtime}
-        key={m.id}
-      />
-  )
-
   return (
     <>
       <div className="flex items-center justify-left w-full m-2 prose-xs text-gray-400 font-light"> {moviesList.length} films</div>
       <main className="prose prose-stone !max-w-none w-full mx-auto">
-        {movies}
+        {moviesList.map(m =>
+          <Movie
+            key={m.id}
+            warning={m.warning}
+            bu={m.bu}
+            title={m.title}
+            plot={m.plot}
+            orig_title={m.orig_title}
+            poster={m.poster}
+            director={m.director}
+            oscar={awards.oscar.map(s => s.toLocaleLowerCase()).includes(m.title.toLocaleLowerCase())}
+            cannes={awards.cannes.map(s => s.toLocaleLowerCase()).includes(m.title.toLocaleLowerCase())}
+            berlin={awards.berlin.map(s => s.toLocaleLowerCase()).includes(m.title.toLocaleLowerCase())}
+            mostra={awards.mostra.map(s => s.toLocaleLowerCase()).includes(m.title.toLocaleLowerCase())}
+            cesar={awards.cesar.map(s => s.toLocaleLowerCase()).includes(m.title.toLocaleLowerCase())}
+            year={m.year}
+            runtime={m.runtime}
+          />
+        )
+        }
       </main>
     </>
   )
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -8,58 +8,53 @@ import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons'
 import { faCompactDisc } from '@fortawesome/free-solid-svg-icons'
 
 
-export default function Movie({warning, bu, title, plot, orig_title, poster, director, oscar, cannes, berlin, mostra, cesar, year, runtime}){
+export default function Movie({ warning, bu, title, plot, orig_title, poster, director, oscar, cannes, berlin, mostra, cesar, year, runtime }) {
 
     let icons = []
-    if(cannes)
+    if (cannes)
         icons.push(<img className="m-0 h-6" src={cannesIcon} alt="cannes icon" />)
-    if(oscar)
+    if (oscar)
         icons.push(<img className="m-0 h-6" src={oscarIcon} alt="oscar icon" />)
-    if(berlin)
+    if (berlin)
         icons.push(<img className="m-0 h-6" src={berlinIcon} alt="berlin icon" />)
-    if(mostra)
+    if (mostra)
         icons.push(<img className="m-0 h-6" src={mostraIcon} alt="mostra icon" />)
-    if(cesar)
+    if (cesar)
         icons.push(<img className="m-0 h-6" src={cesarIcon} alt="cesar icon" />)
 
     let warningDiv = null
-    if(warning !== "")
-        warningDiv = <span className="badge badge-outline badge-error !prose-xs"> 
-                            <FontAwesomeIcon icon={faTriangleExclamation} /> &nbsp; {warning}
-                    </span>
+    if (warning !== "")
+        warningDiv = <span className="badge badge-outline badge-error !prose-xs">
+            <FontAwesomeIcon icon={faTriangleExclamation} /> &nbsp; {warning}
+        </span>
     let buDiv = null
-    if(bu)
-        buDiv = <span className="badge badge-outline badge-accent !prose-xs"> 
-                            <FontAwesomeIcon icon={faCompactDisc} /> &nbsp; B.U
-                    </span>
+    if (bu)
+        buDiv = <span className="badge badge-outline badge-accent !prose-xs">
+            <FontAwesomeIcon icon={faCompactDisc} /> &nbsp; B.U
+        </span>
 
-    return <>
-        <div className="bg-base-100 shadow-lg border border-slate-200 flex flex-col sm:flex-row items-center md:items-start justify-items-center grow-0 my-4 rounded-lg overflow-hidden">
-            <figure className="my-4 sm:!my-0 ">
-                <img src={poster} className="!my-0 max-w-36 grow-0 shrink-0 rounded-lg sm:rounded-none" alt={"poster " + title}/>
-            </figure>
-            <div className="px-4 p-4 flex flex-col items-start">
-                <div className="flex flex-row gap-2">
-                    {warningDiv}
-                    {buDiv}
+    return (<div className="bg-base-100 shadow-lg border border-slate-200 flex flex-col sm:flex-row items-center md:items-start justify-items-center grow-0 my-4 rounded-lg overflow-hidden">
+                <figure className="my-4 sm:!my-0 ">
+                    <img src={poster} className="!my-0 max-w-36 grow-0 shrink-0 rounded-lg sm:rounded-none" alt={"poster " + title} />
+                </figure>
+                <div className="px-4 p-4 flex flex-col items-start">
+                    <div className="flex flex-row gap-2">
+                        {warningDiv}
+                        {buDiv}
+                    </div>
+                    <h2 className="prose-xl font-bold flex items-start gap-2 m-0 flex-wrap text-left">
+                        <span>{title}</span>
+                        <span className="flex">
+                            {icons}
+                        </span>
+                    </h2>
+                    {title.toLowerCase().replace(/[\s\W]+/g, '') !== orig_title.toLowerCase().replace(/[\s\W]+/g, '') ?
+                        <span className="prose-xs text-gray-400 font-light mb-1">({orig_title})</span>
+                        :
+                        ""
+                    }
+                    <div className="prose-sm text-left font-medium">{director}, {year}, {runtime} min</div>
+                    <div className="prose-sm text-left mt-2">{plot}</div>
                 </div>
-                <h2 className="prose-xl font-bold flex items-start gap-2 m-0 flex-wrap text-left">
-                    <span>{title}</span>
-                    <span className="flex">
-                        {icons}
-                    </span>
-                </h2>
-                {title.toLowerCase().replace(/[\s\W]+/g, '') !== orig_title.toLowerCase().replace(/[\s\W]+/g, '') ? 
-                    <span className="prose-xs text-gray-400 font-light mb-1">({orig_title})</span>
-                    :
-                    ""
-                }
-                <div className="prose-sm text-left font-medium">{director}, {year}, {runtime} min</div>
-                <div className="prose-sm text-left mt-2">{plot}</div>
-            </div>
-
-           
-
-        </div>
-    </>
+            </div>)
 }
